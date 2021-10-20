9a51fdae00d64f54af9488df937265d74ec4e5cb
Hlukhanych Bogdan <bodya21111@gmail.com>
2021-10-20 18:12:20 +0300

    fix static

--- app.js
+++ app.js
@@ -9,13 +9,12 @@ const OrdersRouter = require('./routes/orders.routes')
 const config = require('config')
 const path = require('path')
 
-
 const app = express()
 
 const PORT = process.env.PORT || 5000
 
 app.use(express.static(__dirname));
-app.use(express.static(path.resolve(__dirname, 'files')))
+app.use(express.static(path.join(__dirname, 'files')))
 app.use(cors())
 app.use(express.json())
 app.use('/api/auth', AuthRouter)
