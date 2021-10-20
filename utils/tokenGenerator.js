b112dcf750ac80af197ab83b47777a3d179c318c
Hlukhanych Bogdan <bodya21111@gmail.com>
2021-10-20 09:49:46 +0300

    fixed token env in generator

--- utils/tokenGenerator.js
+++ utils/tokenGenerator.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken')
+const config = require('config')
 
 function tokenGenerator(id, role) {
     const payload = {
         id,
         role,
     }
-    return jwt.sign(payload, process.env.SECRET, {expiresIn: 60})
+    return jwt.sign(payload, config.get('SECRET'), {expiresIn: 60})
 }
 
 module.exports = tokenGenerator
