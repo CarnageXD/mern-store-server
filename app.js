9ae8bb3a7e33f8aa7ace605d1655d01dce8a79a7
Hlukhanych Bogdan <bodya21111@gmail.com>
2021-10-29 17:40:45 +0300

    commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const express = require('express')
+const path = require('path')
 const AuthRouter = require('./routes/auth.routes')
 const ProductRouter = require('./routes/product.routes')
 const CartRouter = require('./routes/cart.routes')
@@ -10,7 +11,7 @@ const config = require('config')
 
 const app = express()
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 app.use(express.static(__dirname));
 app.use(cors())
@@ -20,6 +21,13 @@ app.use('/api/products', ProductRouter)
 app.use('/api/cart', CartRouter)
 app.use('/api/orders', OrdersRouter)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../client/build'))
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+}
+
 const start = async () => {
     try {
         await mongoose.connect(config.get("MONGO"))
@@ -27,10 +35,6 @@ const start = async () => {
     } catch (e) {
         console.log('Server error', e.message)
     }
-}
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('/build'))
 }
 
 start()
